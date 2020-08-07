4f72a8f661fadeaa3cb890aff5e8da6a03091a28
Daniel Libatique <daniel.libatique@gmail.com>
2020-08-07 14:08:35 -0400

    filled out most of course info

--- clas199/urls.py
+++ clas199/urls.py
@@ -4,11 +4,13 @@ from . import views
 app_name = 'clas199'
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.IndexView.as_view(), name="index"),
     path('resources', views.resources, name="resources"),
     path('policies', views.policies, name="policies"),
     path('grading', views.grading, name="grading"),
     path('creative_projects', views.creative_projects, name="creative_projects"),
     path('schedule', views.schedule, name="schedule"),
     path('upload_portal', views.upload_portal, name="upload_portal"),
+    path('essay', views.essay, name="essay"),
+    path('citations', views.citations, name="citations"),
 ]
--- clas199/views.py
+++ clas199/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 course_context = {
-    'course_number': 'CLAS 199',
+    'department': 'CLAS',
+    'course_number': 199,
     'course_title': 'Intro to Greco-Roman Gender and Sexuality',
     'base_url': '/CLAS199-F20',
     'help': 'help',
 }
 
 # Create your views here.
-def index(request):
+class IndexView(TemplateView):
 
-    course_context['page_title'] = 'Home'
+    template_name = 'clas199/index.html'
 
-    return render(request, 'clas199/index.html', context=course_context)
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Home'
+
+        # get course context
+        for k in course_context:
+            context[k] = course_context[k]
+
+        return context
 
 def resources(request):
 
@@ -24,28 +34,40 @@ def policies(request):
 
     course_context['page_title'] = 'Policies'
 
-    return render(request, 'clas199/page.html', context=course_context)
+    return render(request, 'clas199/policies.html', context=course_context)
 
 def grading(request):
 
     course_context['page_title'] = 'Grading'
 
-    return render(request, 'clas199/page.html', context=course_context)
+    return render(request, 'clas199/grading.html', context=course_context)
 
 def creative_projects(request):
 
     course_context['page_title'] = 'Creative Projects'
 
-    return render(request, 'clas199/page.html', context=course_context)
+    return render(request, 'clas199/creative_projects.html', context=course_context)
 
 def schedule(request):
 
     course_context['page_title'] = 'Schedule'
 
-    return render(request, 'clas199/page.html', context=course_context)
+    return render(request, 'clas199/schedule.html', context=course_context)
 
 def upload_portal(request):
 
     course_context['page_title'] = 'Upload Portal'
 
     return render(request, 'clas199/page.html', context=course_context)
+
+def essay(request):
+
+    course_context['page_title'] = 'Essay Tips'
+
+    return render(request, 'clas199/essay.html', context=course_context)
+
+def citations(request):
+
+    course_context['page_title'] = 'Citation Examples'
+
+    return render(request, 'clas199/citation.html', context=course_context)
